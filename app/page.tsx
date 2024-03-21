4d1f1978d73b608aa5a2ecf520a3ae7ff2537b71
jeypzz <jeypsruelo@gmail.com>
2024-03-21 09:14:18 +0800

    Build the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import NavBar from "./NavBar";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <NavBar />
+      <main>
+        <div>Hello world</div>
+      </main>
+    </>
+  );
 }
