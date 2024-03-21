974759a36fe3fab989bc8d1b5d14d694f0e50072
jeypzz <jeypsruelo@gmail.com>
2024-03-21 15:15:37 +0800

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField, Text } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 
 import { Controller, useForm } from "react-hook-form";
@@ -8,14 +8,22 @@ import { useRouter } from "next/navigation";
 import "easymde/dist/easymde.min.css";
 import { CiCircleInfo } from "react-icons/ci";
 import { useState } from "react";
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { z } from "zod";
+import { createIssueSchema } from "@/app/ValidationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl space-y-3">
@@ -43,6 +51,11 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors && (
+          <Text color="red" as="p">
+            {errors.title?.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -50,6 +63,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Add a description…" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit Issue</Button>
       </form>
     </div>
